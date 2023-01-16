66c39f1a90a62361821fd2768dc62d539b9d5f29
msaisri1 <122692986+msaisri1@users.noreply.github.com>
2023-01-17 02:54:41 +0530

    Updated Unit test code

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,6 @@ COPY . /app
 
 RUN pip install -r requirements.txt
 
-RUN python -m pip install flask-sqlalchemy
-
 EXPOSE 5000
 
 CMD python ./main.py
--- test_main.py
+++ test_main.py
@@ -13,12 +13,14 @@ def client():
         yield client
 
 
-# def test_save_all_employees(client):
-#     response = client.get('/api/v1/save_all_employees',content_type='application/json')
-#     data = json.loads(response.get_data(as_text=True))
-#     assert response.status_code == 200
-#     assert data['message'] == 'Employees added to database'
-#     assert data['result'] is not None
+def test_save_all_employees(client):
+     response = client.get('/api/v1/save_all_employees',content_type='application/json')
+     data = json.loads(response.get_data(as_text=True))
+     assert response.status_code == 200
+     assert data['message'] == 'Employees added to database'
+     assert data['result'] is not None
+     
+     
 
 
 def test_get_all_employees(client):
@@ -26,20 +28,20 @@ def test_get_all_employees(client):
     data = json.loads(response.get_data(as_text=True))
     assert response.status_code == 200
     assert data is not None
+
     
 
 def test_get_employee_by_id(client):
-    response = client.get('/api/v1/all_employee_by_id/1',content_type='application/json')
+    response = client.get('/api/v1/all_employee_by_id/49',content_type='application/json')
     data = json.loads(response.get_data(as_text=True))
     assert response.status_code == 200
     assert data is not None
-
-
-def test_delete_employee(client):
-    response = client.delete('/api/v1/delete_employee/60',content_type='application/json')
-    data = json.loads(response.get_data(as_text=True))
-    assert response.status_code == 200
-    assert data['message'] == 'Employee deleted'
+    assert data["avatar"] == "https://s3.amazonaws.com/uifaces/faces/twitter/mirfanqureshi/128.jpg"
+    assert data["createdAt"] == "2021-05-04T22:19:31.831Z"
+    assert data["name"] == "Karine Schaefer"
+    assert data["payments"] == "[]"   
+    assert data["id"] == 49
+    
 
 
 def test_update_employee(client):
@@ -47,8 +49,43 @@ def test_update_employee(client):
         "name": "John Smith",
         "createdAt": "2022-10-10T08:40:51.620Z",
         "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/craigdennis/128.jpg",
+        "payments":"1234",
     }
-    response = client.put('/api/v1/update_employee/61',data=json.dumps(employee),content_type='application/json')
+    response = client.put('/api/v1/update_employee/49',data=json.dumps(employee),content_type='application/json')
     data = json.loads(response.get_data(as_text=True))
     assert response.status_code == 200
-    #assert 'Employee not found' in data['message']
+    assert data["avatar"] == "https://s3.amazonaws.com/uifaces/faces/twitter/craigdennis/128.jpg"
+    assert data["createdAt"] == "2022-10-10T08:40:51.620Z"
+    assert data["name"] == "John Smith"
+    assert data["payments"] == "1234"
+
+def test_get_updated_employee_by_id(client):
+    response = client.get('/api/v1/all_employee_by_id/49',content_type='application/json')
+    data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 200
+    assert data is not None
+    assert data["name"] == "John Smith"
+    assert data["avatar"] == "https://s3.amazonaws.com/uifaces/faces/twitter/craigdennis/128.jpg"
+    assert data["createdAt"] == "2022-10-10T08:40:51.620Z"
+    assert data["payments"] == "1234"
+    assert data["id"] == 49
+
+
+def test_delete_employee(client):
+    response = client.delete('/api/v1/delete_employee/49',content_type='application/json')
+    data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 200
+    assert data['message'] == 'Employee deleted'
+
+
+
+def test_update_deleted_employee(client):
+    employee = {
+        "name": "John Smith",
+        "createdAt": "2022-10-10T08:40:51.620Z",
+        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/craigdennis/128.jpg",
+    }
+    response = client.put('/api/v1/update_employee/49',data=json.dumps(employee),content_type='application/json')
+    data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 404
+    assert data["message"] == "Employee not found"
